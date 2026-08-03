ccb72a7667a8c30a4267918d1b5d9cf79df549da
github-action-benchmark <github@users.noreply.github.com>
2026-08-03 18:05:27 +0000

    add certminder benchmarks (pytest) benchmark result for d6285decdd3bc1c9842adee5b8701adda38288ae

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785532948396,
+  "lastUpdate": 1785780327877,
   "repoUrl": "https://github.com/mangrisano/certminder",
   "entries": {
     "certminder benchmarks": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00003390741762097421",
             "extra": "mean: 779.6181487038881 usec\nrounds: 733"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michele.angrisano@gmail.com",
+            "name": "Michele Angrisano",
+            "username": "mangrisano"
+          },
+          "committer": {
+            "email": "michele.angrisano@gmail.com",
+            "name": "Michele Angrisano",
+            "username": "mangrisano"
+          },
+          "distinct": true,
+          "id": "d6285decdd3bc1c9842adee5b8701adda38288ae",
+          "message": "chore(release): 1.7.0",
+          "timestamp": "2026-08-03T20:04:54+02:00",
+          "tree_id": "518eda12dd8be27982c8a3f2b9846be62fdb21e7",
+          "url": "https://github.com/mangrisano/certminder/commit/d6285decdd3bc1c9842adee5b8701adda38288ae"
+        },
+        "date": 1785780326756,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/bench_perf.py::test_detect_problems",
+            "value": 173322.51449296216,
+            "unit": "iter/sec",
+            "range": "stddev: 6.178957059140644e-7",
+            "extra": "mean: 5.769590886246954 usec\nrounds: 20168"
+          },
+          {
+            "name": "benchmarks/bench_perf.py::test_detect_problems_batch",
+            "value": 1668.1114293677185,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000008902180552030672",
+            "extra": "mean: 599.4803359024045 usec\nrounds: 908"
           }
         ]
       }
